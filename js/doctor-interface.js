706135c8cc020e516752db6a43ad72fdbe99388b
Dylan Dills <dylan.dills@gmail.com>
2017-06-30 16:51:27 -0700

    continued refactoring ofinterface, as well as backend, so doctor array can display

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,10 +1,14 @@
 var Doctors = require('./../js/doctor.js').docModule;
 
+var displayDoctor = function(doctor) {
+  $('.showDoctor').text("Here's your doctor: " + doctor);
+};
+
 $(document).ready(function() {
   var currentDoctorObject = new Doctors();
   $('#better-doctor').click(function() {
     var medicalIssue = $('#symptom').val();
     $('#symptom').val("");
-    currentDoctorObject.getDoctors(medicalIssue);
+    currentDoctorObject.getDoctors(medicalIssue, displayDoctor);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -3,10 +3,11 @@ var apiKey = require('./../.env').apiKey;
 function Doctors(){
 }
 
-Doctors.prototype.getDoctors = function(medicalIssue) {
+Doctors.prototype.getDoctors = function(medicalIssue, displayDoctor) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
  .then(function(result) {
-    console.log(result);
+   displayDoctor(medicalIssue, result.practices[2]);
+    console.log(displayDoctor);
   })
  .fail(function(error){
     console.log("fail");
