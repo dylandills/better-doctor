d57e27b57e77f685cefe141ef39cb5fafd5dfd82
Dylan Dills <dylan.dills@gmail.com>
2017-07-06 21:32:58 -0700

    user interface functionality with for loops, as well as list style items for first name, last name, phone number, address, and if the doctor accepts new patients or not

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,14 +1,21 @@
 var Doctors = require('./../js/doctor.js').docModule;
 //
-function displayDoctor (doctor) {
-    $('.showDoctor').text("MD: " + doctor.name + "Phone Number: " + doctor.phone + "Accepting New Patients: " + doctor.accepting );
+var displayDoctor = function(myDoctors) {
+  for(i = 0; i < myDoctors.length; i++ ) {
+$('.showDoctor').append(
+  "<ul><li><h3>MD: " + myDoctors[i].first_name + " " + myDoctors[i].last_name + "</li></h3>"
++ "<li><h3>Phone Number: " + myDoctors[i].phones + "</h3></li>"
++ "<li><h3>Address: "+ myDoctors[i].visit_address + "</h3></li>"
++ "<li><h3>Accepting New Patients?: " + myDoctors[0].accepts_new_patients) +  "</h3></li></ul>"
+  }
 };
 
 $(document).ready(function() {
     var currentDoctorObject = new Doctors();
     $('#better-doctor').click(function() {
+      $('.showDoctor').empty();
         var medicalIssue = $('#symptom').val();
         $('#symptom').val("");
-        displayDoctor(currentDoctorObject.getDoctors(doctor));
+        currentDoctorObject.getDoctors(medicalIssue, displayDoctor);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -4,18 +4,25 @@ function Doctors() {
 
 }
 
-function transformDoctor(doctor){
-  return {
-    name: doctor.name,
-    phone: doctor.phones[0].number,
-    accepting: doctor.accepts_new_patients
-  }
+function Doctor(first_name, last_name, phones, visit_address, accepts_new_patients){
+  this.first_name = first_name;
+  this.last_name = last_name;
+  this.phones = phones;
+  this.visit_address = visit_address;
+  this.accepts_new_patients = accepts_new_patients;
 }
 
 Doctors.prototype.getDoctors = function(medicalIssue, displayDoctor) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
  .then(function(result) {
-   return transformDoctor(result.data[0].practices[0]);
+   var myDoctors = [];
+   var doctorInfo = result.data;
+   for(i = 0; i < doctorInfo.length; i++) {
+      var newDoctor = new Doctor(result.data[i].profile.first_name, result.data[i].profile.last_name, result.data[i].practices[0].phones[0].number, result.data[i].practices[0].visit_address.street,
+      result.data[i].practices[0].accepts_new_patients);
+      myDoctors.push(newDoctor);
+    }
+    displayDoctor(myDoctors);
   })
  .fail(function(error){
     console.log("fail");
